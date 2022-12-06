b750b9e9fc883ca650aed47377e5cb3e5194ecb4
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2022-12-06 08:55:17 +0700

    Bắt sự kiện cho các item trong menu

--- Menu/MenuBinhThuong/app/src/main/java/com/example/menukhoitao/MainActivity.java
+++ Menu/MenuBinhThuong/app/src/main/java/com/example/menukhoitao/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.menukhoitao;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,31 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_demo, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuCuChi:
+                Toast.makeText(this, "Bạn đã click cữ chỉ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuGiaoDien:
+                Toast.makeText(this, "Bạn đã click giao diện", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuHeThong:
+                Toast.makeText(this, "Bạn đã click hệ thống", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuExit:
+                Toast.makeText(this, "Bạn đã click exit", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuSearch:
+                Toast.makeText(this, "Bạn đã click search", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuShare:
+                Toast.makeText(this, "Bạn đã click share", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
